d149fb0c50ffad93bf542725f117f763d85836ed
JoseGlez23 <josecmata234@gmail.com>
2026-02-21 18:26:05 -0700

    Condiciones aplicadas

--- src/main.py
+++ src/main.py
@@ -13,4 +13,6 @@ class Calculator:
         return a * b  
 
     def divide(self, a: int, b: int) -> float:
+        if b == 0:  #Condición de cero para evitar división por cero
+            raise ValueError("No se puede dividir por cero")
         return a / b  
